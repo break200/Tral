90699dec9e50b11d2328ccef94c1d7178b0ac586
sanghee <sanghee.son@promr.co.kr>
2024-04-17 11:42:28 +0900

    Initial Commit

---
+++ Tral/TralApp.swift
@@ -0,0 +1,17 @@
+//
+//  TralApp.swift
+//  Tral
+//
+//  Created by break200 on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
