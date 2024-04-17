d4b16dbebab19ebe002f7c46f270c91a4109c496
sanghee <sanghee.son@promr.co.kr>
2024-04-17 11:47:22 +0900

    splashview 추가 및 이미지 추가

---
+++ Tral/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Tral
+//
+//  Created by break200 on 4/17/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
